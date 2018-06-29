7487b518257bfa2ed09992fe44d3eb3f84f5baf5
Esteban Segura Benavides <esteban.1703@gmail.com>
2018-06-29 10:33:36 -0600

    Init commit

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+
+    'secret': 'SECRETPASSWORDKEY',
+    'database': 'mongodb://localhost:27017/ComercioyNegociosDigitales',
+    'databaseProd': 'mongodb://essegura:hola123hola@ds113169.mlab.com:13169/comercioynegociosdigitales'
+
+};
